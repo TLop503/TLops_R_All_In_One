60e96a30c3cbaae74c6dec023d62aaa86a92205f
Troy Lopez <troy.n.lopez@gmail.com>
2023-04-07 17:49:50 -0700

    branched

---
+++ Troys_All_In_One.R
@@ -0,0 +1,19 @@
+# All in one Script to rip commonly used functions quickly
+
+#By Troy Lopez / TLop503 on github
+#Protected by the Gnu General Public License v3.0
+
+# assumes you have installed tidyverse and openintro packages
+#loads libraries incase you haven't yet
+#please run the following commands before running this script
+#library(openintro)
+#library(tidyverse)
+
+print("Please enter the number 1 for a vector and 0 for a column of a dataset")
+var = readline();
+var = as.integer(var);
+print(var)
+
+if (var == 1) {
+  
+}
