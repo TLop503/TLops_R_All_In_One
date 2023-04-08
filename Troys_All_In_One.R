4727291de0fb1822ca7d0ca6305077ab42ad5312
Troy Lopez <troy.n.lopez@gmail.com>
2023-04-08 13:28:32 -0700

    fancy new markdown formatting for instructions

--- Troys_All_In_One.R
+++ Troys_All_In_One.R
@@ -9,10 +9,12 @@ library(tidyverse)
 
 # CHANGE THE FOLLOWING LINES TO REFLECT YOUR DATA SET
 #LOADING IN DATA (If you haven't yet)
-# data(example) uncomment this line and plug in the name of your data if it's in a package
+data("loan50") #this is an example line, delete before running your code
+# data("example") uncomment this line and plug in the name of your data if it's in a package
 # data <- read.csv("file_name.csv") #uncomment this line and plug in file name if needed
 
 #SET UP COLUMN OR VECTOR
+var <- loan50$loan_amount #this is an example line, delete or edit before running
 #var <- example$column_name #uncomment this line and plug in data set and column
 #var <- vector_name #uncomment this line and plug in vector name if needed
 
@@ -21,3 +23,4 @@ mean(var)
 median(var)
 IQR(var)
 quantile(var)
+
