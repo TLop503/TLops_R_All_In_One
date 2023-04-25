9e1b8650c69532d58fbc3df7608478a2632c9255
Troy Lopez <troy.n.lopez@gmail.com>
2023-04-25 00:22:51 -0700

    Completed code for week 3 part 1

---
+++ TLopsWeek3Part1.R
@@ -0,0 +1,12 @@
+#Follows all the default code snippets from
+#Data Analyses 3
+#To avoid cheating this script only includes
+#Things everyone will have to do, just as a precaution
+#So I don't get in trouble for this.
+
+wheel_outcomes = c(rep(0,2),  rep(300,5),
+                   rep(350,1),rep(400,2), 
+                   rep(450,1),rep(500,3), 
+                   rep(550,1),rep(600,3), 
+                   rep(700,1),rep(800,2), 
+                   rep(900,2),rep(5000,1))
